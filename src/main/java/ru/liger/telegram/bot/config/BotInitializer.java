61ce7bfaf62bb52ec809341d82d001e757164b39
Anton Suhankin <anton.sukhankin@mail.ru>
2024-10-16 23:07:16 +0300

    Refactor + template

--- src/main/java/ru/liger/telegram/bot/config/BotInitializer.java
+++ src/main/java/ru/liger/telegram/bot/config/BotInitializer.java
@@ -8,19 +8,19 @@ import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
-import ru.liger.telegram.bot.service.TelegramBot;
+import ru.liger.telegram.bot.facade.TelegramBotFacade;
 
 @Component
 @AllArgsConstructor
 @Slf4j
 public class BotInitializer {
-    private final TelegramBot telegramBot;
+    private final TelegramBotFacade telegramBotFacade;
 
     @EventListener({ContextRefreshedEvent.class})
     public void init() throws TelegramApiException {
         var telegramBotApi = new TelegramBotsApi(DefaultBotSession.class);
         try {
-            telegramBotApi.registerBot(telegramBot);
+            telegramBotApi.registerBot(telegramBotFacade);
         } catch (Exception exception) {
             log.error("catch error on initlz: {}", exception.getMessage());
         }
--- src/main/java/ru/liger/telegram/bot/facade/TelegramBotFacade.java
+++ src/main/java/ru/liger/telegram/bot/facade/TelegramBotFacade.java
@@ -1,4 +1,4 @@
-package ru.liger.telegram.bot.service;
+package ru.liger.telegram.bot.facade;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -9,19 +9,25 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import ru.liger.telegram.bot.config.BotConfig;
 import ru.liger.telegram.bot.entity.TelegramUser;
+import ru.liger.telegram.bot.service.TelegramUserService;
 
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
 @Service
 @AllArgsConstructor
-@Slf4j
-public class TelegramBot extends TelegramLongPollingBot {
+public class TelegramBotFacade extends TelegramLongPollingBot {
 
     private final BotConfig botConfig;
 
     private final TelegramUserService telegramUserService;
 
+    private final Map<Long, String> userState = new HashMap<>();
+
     @Override
     public void onUpdateReceived(Update update) {
-        log.info("uodate recieved");
+        log.info("update received - {}", update);
 
         if (!update.hasMessage() || !update.getMessage().hasText()) {
             return;
@@ -30,7 +36,8 @@ public class TelegramBot extends TelegramLongPollingBot {
         var chatId = update.getMessage().getChatId();
         var userText = update.getMessage().getText();
         var username = update.getMessage().getChat().getUserName();
-        log.info("info: chatod: {}\\n msgtext: {}\n username {}\n", chatId, userText, username);
+
+        log.info("Bot received message. ChatId: {}\\n msgtext: {}\n username {}\n", chatId, userText, username);
 
         switch (userText) {
             case "/start":
@@ -40,10 +47,17 @@ public class TelegramBot extends TelegramLongPollingBot {
                         .build());
                 startCommandReceived(username, chatId);
                 break;
+            case "/startNewDialog":
+                createNewDialogWithUser(username, chatId);
+                break;
             default:
-                //logs
+                log.warn("Unknown command message {}. User = {}, chatId = {}", userText, username, chatId);
                 sendMessage(chatId, "The command was not recognised");
         }
+
+    }
+
+    private void createNewDialogWithUser(String username, Long chatId) {
 
     }
 
@@ -64,15 +78,17 @@ public class TelegramBot extends TelegramLongPollingBot {
         sendMessage(chatId, responseToUser);
     }
 
-    private void sendMessage(long chatId, String messageText) {
+    private void sendMessage(Long chatId, String userMessage) {
         var message = new SendMessage();
         message.setChatId(String.valueOf(chatId));
-        message.setText(messageText);
+        message.setText(userMessage);
+        sendMessage(message);
+    }
 
+    private void sendMessage(SendMessage message) {
         try {
             execute(message);
         } catch (TelegramApiException e) {
-            // add logs
             throw new RuntimeException(e);
         }
 
